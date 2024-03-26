8527d2975235bdc4500194ddb9e6ff695c4ff9bf
JEKKY <137982621+vJEKKYv@users.noreply.github.com>
2024-03-27 00:35:11 +0900

    Create chap07_B02.cpp

---
+++ homework/chap07_B02.cpp
@@ -0,0 +1,13 @@
+# Nucloe의 A0에서 A5핀의 전압을 0.5초마다 차례로 ADC 하여 PC에 16비트 정수로 표시
+# 하는 코드를 작성하라. 단, 객체의 배열을 사용한다. 
+#include "mbed.h"
+AnalogIn adc[5] = {A0,A2,A3,A4,A5};
+int main()
+{
+    while(true){
+        for(int i = 0; i < 5; i++){
+            printf("A%d = %u V\t",i,adc[i].read_u16());
+        }
+        printf("\n");        ThisThread::sleep_for(500ms);
+    }
+}
