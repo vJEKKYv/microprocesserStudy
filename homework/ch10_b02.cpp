01a7efd165550466aa9dd6756aac234a4b9f735b
JEKKY <137982621+vJEKKYv@users.noreply.github.com>
2024-04-06 18:48:04 +0900

    ch10_b02.cpp 문제 삽입

--- homework/ch10_b02.cpp
+++ homework/ch10_b02.cpp
@@ -1,3 +1,5 @@
+//Timer 클래스를 이용하여 𝑥 값이 1, 2, ... 10000까지의 변할 때 아래와 같은 RMS(root meansquare)를 구하는데 소요되는 시간을 us단위로 PC에 출력하는 프로그램을 작성하라. 단, computeRMS라는 함수로 만들어 RMS값을 반환 받도록 한다. 
+//rms=입력 값들을 각각 제곱, 그 수를 다 더한 것을 전체 제곱근. (like L2 norm 연산)
 #include "mbed.h"
 #include "math.h"
 #define SAMPLE_INTERVAL 10000
